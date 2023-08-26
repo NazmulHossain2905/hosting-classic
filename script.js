9c8bd9bd441b0fe0fb36b56f7f6d614d3138569a
Nazmul_Hossain <nazmulhossain2905@gmail.com>
2023-08-26 15:56:20 +0600

    Hosting classic site design complete

---
+++ script.js
@@ -0,0 +1,43 @@
+console.log("Hello world!");
+let abg = "abcdefghijklmnopqrstuvwxyz";
+console.log(abg.substring(1));
+// font family bold are to avoid
+function fontFamily() {
+  let bold = "bold";
+  console.log(bold);
+
+  if (bold == "bold") {
+    console.log("ok bold");
+  }
+}
+
+if (bold == "bold") {
+  console.log("ok bold");
+}
+
+console.log("Hello world!");
+
+let myName = "Nazmul Hossain";
+console.log(myName);
+
+let i = document.getElementById("hello");
+let name = i.getAttribute("name");
+
+function funcName() {
+  let aNumber = 10;
+  aNumber += 1;
+}
+
+// how to declare a function
+function func() {}
+
+// hwy tabnine ai how to declare a object?
+let object = {
+  name: "John",
+  age: "16",
+  isMarried: true,
+  phone: "123",
+  country: "US",
+  email: "john@example.com",
+  password: "password",
+};
